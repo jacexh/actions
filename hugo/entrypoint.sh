6322ef19f9b88f6710b07e684518f714e3729e77
Xu Hao <jace.xh@gmail.com>
2020-03-11 12:05:02 +0800

    Update entrypoint.sh

--- hugo/entrypoint.sh
+++ hugo/entrypoint.sh
@@ -6,12 +6,12 @@ git config --global user.email "$EMAIL"
 git config --global user.name "$GITHUB_ACTOR"
 
 echo "begin to clone hugo source code"
-git clone --recursive https://$GITHUB_ACTOR:${{ secrets.GITHUB_TOKEN }}@github.com/$GITHUB_REPOSITORY.git blog
+git clone --recursive https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git blog
 cd blog
 rm -rf public
 
 echo "begin to clone github page repository"
-git clone --recursive https://$GITHUB_ACTOR:${{ secrets.GITHUB_TOKEN }}@github.com/$GITHUB_ACTOR/$GITHUB_ACTOR.github.io.git public
+git clone --recursive https://$GITHUB_ACTOR:$GITHUB_TOKE@github.com/$GITHUB_ACTOR/$GITHUB_ACTOR.github.io.git public
 
 echo "building hugo application"
 hugo
